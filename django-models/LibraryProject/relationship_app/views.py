3384e7043b5acbe01da6d451aa03ce895561ebc0
kaberege <kabgnestor@gmail.com>
2024-11-05 11:41:13 +0200

    added new file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Book, Library
+from .models import Book
+from .models import Library
 from django.views.generic import DetailView
 
 def hello_view(request):
@@ -17,5 +18,5 @@ def book_list(request):
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/list_books.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
