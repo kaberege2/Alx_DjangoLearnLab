0cd16162a958abf311d1155a19a230303f050e5e
kaberege <kabgnestor@gmail.com>
2024-11-05 11:35:27 +0200

    added modifications in templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -12,10 +12,10 @@ def book_list(request):
       """Retrieves all books and renders a template displaying the list."""
       books = Book.objects.all()  # Fetch all book instances from the database
       context = {'book_list': books}  # Create a context dictionary with book list
-      return render(request, 'list_books.html', context)
+      return render(request, 'relationship_app/list_books.html', context)
 
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/list_books.html'
     context_object_name = 'library'
