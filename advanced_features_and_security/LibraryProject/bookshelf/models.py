477adc50a97456c320a1989adaa590da434f59fa
kaberege <kabgnestor@gmail.com>
2024-11-12 15:37:05 +0200

    added project

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -5,22 +5,23 @@ from django.contrib.auth.models import BaseUserManager
 from django.conf import settings
 
 # Create your models here.
+ ["class CustomUser(AbstractUser):", "date_of_birth", "profile_photo"]
 
-class Book(models.Model):
+class BookModel(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
 
 
-class Author(models.Model):
+class Authore(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
-class Book(models.Model):
+class Books(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    author = models.ForeignKey(Authore, on_delete=models.CASCADE, related_name='books')
     
     class Meta:
         permissions = [
@@ -32,21 +33,21 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
-class Library(models.Model):
+class Librarys(models.Model):
     name = models.CharField(max_length=100)
-    books = models.ManyToManyField(Book, related_name='libraries')
+    books = models.ManyToManyField(Books, related_name='libraries')
 
     def __str__(self):
         return self.name
 
-class Librarian(models.Model):
+class Librarians(models.Model):
     name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
+    library = models.OneToOneField(Librarys, on_delete=models.CASCADE, related_name='librarian')
 
     def __str__(self):
         return self.name
 
-class CustomUser(AbstractUser):
+class CustomUsers(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
@@ -54,7 +55,7 @@ class CustomUser(AbstractUser):
         return self.username
         
 #define a UserProfile with a role field:
-class UserProfile(models.Model):
+class UserProfiles(models.Model):
     ROLE_CHOICES = (
         ('Admin', 'Admin'),
         ('Librarian', 'Librarian'),
@@ -67,8 +68,8 @@ class UserProfile(models.Model):
         return self.user.username
 
 
-class CustomUserManager(BaseUserManager):
-    def create_user(self, username, email, password=None, **extra_fields):
+class CustomUserManagers(BaseUserManager):
+    def create_users(self, username, email, password=None, **extra_fields):
         if not email:
             raise ValueError("The Email field must be set")
         email = self.normalize_email(email)
@@ -77,7 +78,7 @@ class CustomUserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, email, password=None, **extra_fields):
+    def create_superusers(self, username, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
