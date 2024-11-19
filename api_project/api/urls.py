4e767d10b2f00e4dab16bcbef5f36b245f16cc39
kaberege <kabgnestor@gmail.com>
2024-11-19 11:46:49 +0200

    added permissions

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import BookList, BookViewSet
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
@@ -9,4 +10,5 @@ urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
      # Include the router URLs for BookViewSet (all CRUD operations)
     path('', include(router.urls)),  # This includes all routes registered with the router
+    path('api-token-auth/', obtain_auth_token),  #Set up an endpoint to retrieve authentication tokens using DRF’s obtain_auth_token view.
 ]
--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',  # <-- Add this line
     'api',
 ]
 
@@ -123,3 +124,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#This tells DRF to use token-based authentication for all API views by default.
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',  # <-- Add this line
+    ],
+}
