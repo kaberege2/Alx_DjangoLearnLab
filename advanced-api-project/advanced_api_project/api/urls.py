f92ae46099ae2c44df3caf0f93584f106a7572fc
kaberege <kabgnestor@gmail.com>
2024-11-26 14:56:43 +0200

    added a new url

--- advanced-api-project/advanced_api_project/api/urls.py
+++ advanced-api-project/advanced_api_project/api/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     path('books/', ListView.as_view(), name='book-list'),  # List all books
     path('books/<int:pk>/', DetailView.as_view(), name='book-detail'),  # Retrieve a specific book by ID
     path('books/create/', CreateView.as_view(), name='book-create'),  # Create a new book
-    path('books/<int:pk>/update/', UpdateView.as_view(), name='book-update'),  # Update an existing book
-    path('books/<int:pk>/delete/', DeleteView.as_view(), name='book-delete'),  # Delete a book
+    path('books/update/', UpdateView.as_view(), name='book-update'),  # Update an existing book
+    path('books/delete/', DeleteView.as_view(), name='book-delete'),  # Delete a book
     path("", include(router.urls)),
     path('api-token-auth/', obtain_auth_token),  #Set up an endpoint to retrieve authentication tokens using DRF’s obtain_auth_token view.
 ]
