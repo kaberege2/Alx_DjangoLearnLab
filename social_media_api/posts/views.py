27c708ef4bc1ce4be8781e4a4e33727ced377c62
kaberege <kabgnestor@gmail.com>
2024-12-10 20:42:30 +0200

    added permissions

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Post, Comment
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .serializers import PostSerializer, CommentSerializer
 from rest_framework import generics, viewsets
 from rest_framework.pagination import PageNumberPagination
@@ -23,6 +23,7 @@ class PostPagination(PageNumberPagination):
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
     pagination_class = PostPagination
      # Enable Filtering, Searching, and Ordering
     filter_backends = [rest_framework.DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
@@ -32,10 +33,27 @@ class PostViewSet(viewsets.ModelViewSet):
     search_fields = ['title', 'content']
     ordering_fields = ['title', 'created_at']
     ordering = ['title']  # Default ordering by title
+    
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def perform_update(self, serializer):
+        # Check if the user is the author before updating
+        if self.get_object().author != self.request.user:
+            raise PermissionDenied("You can only update your own posts.")
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        # Check if the user is the author before deleting
+        if instance.author != self.request.user:
+            raise PermissionDenied("You can only delete your own posts.")
+        instance.delete()
 
 class CommentViewset(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    #permission_classes = [IsAuthenticated]
     pagination_class = PostPagination
      # Enable Filtering, Searching, and Ordering
     filter_backends = [rest_framework.DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
@@ -44,3 +62,18 @@ class CommentViewset(viewsets.ModelViewSet):
     search_fields = ['content']
     ordering_fields = ['content', 'created_at']
     ordering = ['content']  # Default ordering by title
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def perform_update(self, serializer):
+        # Check if the user is the author before updating
+        if self.get_object().author != self.request.user:
+            raise PermissionDenied("You can only update your own posts.")
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        # Check if the user is the author before deleting
+        if instance.author != self.request.user:
+            raise PermissionDenied("You can only delete your own posts.")
+        instance.delete()
